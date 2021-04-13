41900939bb22e5ad5e57b4ed880f6bcfc308deb2
Rong Ou <rong.ou@gmail.com>
2021-04-12 20:38:17 -0700

    only push with X11 display

--- cudf-refresh.sh
+++ cudf-refresh.sh
@@ -19,7 +19,9 @@ conda deactivate
 git co branch-"${CUDF_VERSION}"
 git fetch upstream
 git rebase upstream/branch-"${CUDF_VERSION}"
-git push
+if [[ -n "${DISPLAY}" ]]; then
+  git push
+fi
 conda env create -f conda/environments/cudf_dev_cuda11.2.yml --force
 conda activate cudf_dev
 conda remove cmake --force -y
