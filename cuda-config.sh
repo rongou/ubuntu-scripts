27ef51a3a957ccd95837a1b9477ecedb1474b23f
Rong Ou <rong.ou@gmail.com>
2021-04-15 09:51:10 -0700

    fix cuda config

--- cuda-config.sh
+++ cuda-config.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-sudo update-alternatives --install /usr/local/cuda cuda /usr/local/cuda-11.2.2 1122
-sudo update-alternatives --install /usr/local/cuda cuda /usr/local/cuda-11.2.1 1121
+sudo update-alternatives --install /usr/local/cuda cuda /usr/local/cuda-11.2 1120
 sudo update-alternatives --install /usr/local/cuda cuda /usr/local/cuda-11.1 1110
 sudo update-alternatives --install /usr/local/cuda cuda /usr/local/cuda-11.0 1100
 sudo update-alternatives --install /usr/local/cuda cuda /usr/local/cuda-10.2 1020
