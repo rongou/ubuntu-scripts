104901536da8d42385f4de3b0712723a95c4646e
Rong Ou <rong.ou@gmail.com>
2022-11-21 17:49:38 -0800

    update gcc 11/12 for ubuntu 22.04

--- gcc-config.sh
+++ gcc-config.sh
@@ -1,27 +1,9 @@
 #!/usr/bin/env bash
 
-sudo update-alternatives\
- --install /usr/bin/gcc gcc /usr/local/gcc-12.1.0/bin/gcc 121\
- --slave /usr/bin/g++ g++ /usr/local/gcc-12.1.0/bin/g++\
- --slave /usr/bin/gcov gcov /usr/local/gcc-12.1.0/bin/gcov
-sudo update-alternatives\
- --install /usr/bin/gcc gcc /usr/local/gcc-11.3.0/bin/gcc-11.3 113\
- --slave /usr/bin/g++ g++ /usr/local/gcc-11.3.0/bin/g++-11.3\
- --slave /usr/bin/gcov gcov /usr/local/gcc-11.3.0/bin/gcov-11.3
-sudo update-alternatives\
- --install /usr/bin/gcc gcc /usr/local/gcc-11.2.0/bin/gcc-11.2 112\
- --slave /usr/bin/g++ g++ /usr/local/gcc-11.2.0/bin/g++-11.2\
- --slave /usr/bin/gcov gcov /usr/local/gcc-11.2.0/bin/gcov-11.2
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 1120\
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-12 1210\
+ --slave /usr/bin/g++ g++ /usr/bin/g++-12 --slave /usr/bin/gcov gcov /usr/bin/gcov-12
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 1130\
  --slave /usr/bin/g++ g++ /usr/bin/g++-11 --slave /usr/bin/gcov gcov /usr/bin/gcov-11
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 100\
- --slave /usr/bin/g++ g++ /usr/bin/g++-10 --slave /usr/bin/gcov gcov /usr/bin/gcov-10
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 90\
- --slave /usr/bin/g++ g++ /usr/bin/g++-9 --slave /usr/bin/gcov gcov /usr/bin/gcov-9
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 80\
- --slave /usr/bin/g++ g++ /usr/bin/g++-8 --slave /usr/bin/gcov gcov /usr/bin/gcov-8
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 70\
- --slave /usr/bin/g++ g++ /usr/bin/g++-7 --slave /usr/bin/gcov gcov /usr/bin/gcov-7
 
 sudo update-alternatives --config gcc
 
