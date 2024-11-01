4684c09e756887f6e02b0347c2f18495127b4d3e
Rong Ou <rong.ou@gmail.com>
2024-11-01 11:14:20 -0700

    update cuda config

--- cuda-config.sh
+++ cuda-config.sh
@@ -1,19 +1,5 @@
 #!/usr/bin/env bash
 
-sudo update-alternatives --install /usr/local/cuda cuda /usr/local/cuda-12.6 1261
-sudo update-alternatives --install /usr/local/cuda cuda /usr/local/cuda-12.5 1251
-sudo update-alternatives --install /usr/local/cuda cuda /usr/local/cuda-12.4 1241
-sudo update-alternatives --install /usr/local/cuda cuda /usr/local/cuda-12.3 1230
-sudo update-alternatives --install /usr/local/cuda cuda /usr/local/cuda-12.2 1220
-sudo update-alternatives --install /usr/local/cuda cuda /usr/local/cuda-12.1 1211
-sudo update-alternatives --install /usr/local/cuda cuda /usr/local/cuda-12.0 1200
-sudo update-alternatives --install /usr/local/cuda cuda /usr/local/cuda-11.8 1180
-sudo update-alternatives --install /usr/local/cuda cuda /usr/local/cuda-11.7 1170
-sudo update-alternatives --install /usr/local/cuda cuda /usr/local/cuda-11.3 1130
-sudo update-alternatives --install /usr/local/cuda cuda /usr/local/cuda-11.2 1120
-sudo update-alternatives --install /usr/local/cuda cuda /usr/local/cuda-11.1 1110
-sudo update-alternatives --install /usr/local/cuda cuda /usr/local/cuda-11.0 1100
-sudo update-alternatives --install /usr/local/cuda cuda /usr/local/cuda-10.2 1020
-sudo update-alternatives --install /usr/local/cuda cuda /usr/local/cuda-10.1 1010
+sudo update-alternatives --install /usr/local/cuda cuda /usr/local/cuda-12.6 1262
 
 sudo update-alternatives --config cuda
