683533290e5ca03da1f5588073ed411f391bf640
Rong Ou <rong.ou@gmail.com>
2025-01-23 15:27:31 -0800

    add cuda 12.8

--- cuda-config.sh
+++ cuda-config.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+sudo update-alternatives --install /usr/local/cuda cuda /usr/local/cuda-12.8 1280
 sudo update-alternatives --install /usr/local/cuda cuda /usr/local/cuda-12.6 1262
 
 sudo update-alternatives --config cuda
